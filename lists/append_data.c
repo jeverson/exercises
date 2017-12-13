36d710413814c32824b88defc56baf242c937a05
Jeverson Morais <jeverson.morais@programmers.com.br>
2017-12-13 17:33:40 -0200

    Get length and nth item

--- lists/append_data.c
+++ lists/append_data.c
@@ -11,16 +11,24 @@ struct node {
 node* create_node(int data);
 void append_data(node** headRef, int data);
 void print_list(node* head);
+void print_node(node* node);
 node* get_last_item(node* head);
+int get_length(node* head);
+node* get_nth(node* head, int n);
 
 int main() {
     printf("Hello World!\n");
 
     node* head = create_node(10);
-
+    get_length(head);
     append_data(&head, 20);
     append_data(&head, 30);
     append_data(&head, 40);
+    get_length(head);
+    get_nth(head, 0);
+    get_nth(head, 1);
+    get_nth(head, 3);
+    get_nth(head, 5);
     printf("head %d\n", head->data);    
     print_list(head);
 
@@ -49,13 +57,47 @@ void append_data(node** headRef, int data) {
     }
 }
 
+int get_length(node* head) {
+    int count = 0;
+    while (head != NULL) {
+        head = head->next;
+        count++;
+    }
+    printf("length: %d\n", count);
+    return count;
+}
+
+node* get_nth(node* head, int n) {
+    assert(n >= 0);
+
+    int p = 0;
+    while (head != NULL && p < n) {
+        head = head->next;
+        p++;
+    }    
+    printf("getting node at %d position\n", n);
+    print_node(head);
+    return head;
+}
+
+
 void print_list(node* head) {
-    printf("Printing list: \n");
+    printf("printing list: \n");
     while (head != NULL) {
         printf("%d->", head->data);
         head = head->next;
     }
     printf("NULL\n");
+}
+
+void print_node(node* node) {
+    if (node != NULL) {
+        printf("data: %d at %p\n", node->data, node);
+    }
+    else 
+    {
+        printf("node is null\n");
+    }
 }
 
 node* get_last_item(node* head) {
